f7b26df4b83151fad726df0fdaefabd3e65f0892
ggssh <2821764200@qq.com>
2021-11-18 18:36:28 +0800

    Style : 优化文件格式

--- lab.srcs/sources_1/new/rom.v
+++ lab.srcs/sources_1/new/rom.v
@@ -21,25 +21,22 @@
 
 
 module rom(
-         input wire ce, //存储器使能信号
-         input wire[5:0] addr,
-         output reg[31:0] inst //指令宽度为32
+           input wire ce, //存储器使能信号
+           input wire[5:0] addr,
+           output reg[31:0] inst //指令宽度为32
        );
 reg[31:0] rom[63:0]; //使用二维向量定义存储器,深度是64,每个元素即为指令,宽度为32位,所以地址选用6位即可
 
 initial begin
-  $readmemh("rom.data",rom);
+    $readmemh("rom.data",rom);
 end
 
-always @(*)
-  begin
-    if(ce == 1'b0)
-      begin
+always @(*) begin
+    if(ce == 1'b0) begin
         inst <= 32'h0; //这里使用十六进制
-      end
-    else
-      begin
+    end
+    else begin
         inst <= rom[addr]; //使能信号有效时,给出地址addr对应的指令
-      end
-  end
+    end
+end
 endmodule
